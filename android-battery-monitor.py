4764b97bd6e2a41950488581dc39551255b27f60
zainosaurus <zain.tahlilkar@gmail.com>
2019-01-08 00:10:08 -0500

    Updated battery monitor app

--- android-battery-monitor.py
+++ android-battery-monitor.py
@@ -4,23 +4,28 @@
 import subprocess
 from datetime import datetime
 import time
+import re
 
 # Holds plot data
 timestamps = []
 voltages = []
 currents = []
 
-while (true):
+while (True):
 	# Get battery info by using a system call
 	battery_stat = subprocess.check_output(['adb', '-d', 'shell', 'dumpsys', 'battery']).decode('utf-8')
 	battery_stat = list(map(lambda el: el.strip(), battery_stat.split('\n')))
 
 	# Find voltage and current & push to array
-	time = datetime.now().time()
+	cur_time = datetime.now().time()
 	for element in battery_stat:
-		if 'voltage' in element
-			print(element)
-		elif 'current' in element
-			print(current)
+		v = re.match(r'voltage: *(\d+)', element)
+		c = re.match(r'current now: *(-?\d+)', element)
+		if v != None:
+			voltage = v.group(1)
+		elif c != None:
+			current = c.group(1)
 
-	time.sleep(30)
+	print(cur_time, voltage, current)
+	time.sleep(5)
+
